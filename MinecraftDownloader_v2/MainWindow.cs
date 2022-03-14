1856dbdc4ce9e72ac00fb6869226a9aed10ccb4a
RoZeroZero <r-berko4@mail.ru>
2022-03-14 22:23:01 +0700

    Revert "update GetVersions();"
    This reverts commit bffaf36fca60ad68c4de42edff5b07a9d22420c7.

--- MinecraftDownloader_v2/MainWindow.cs
+++ MinecraftDownloader_v2/MainWindow.cs
@@ -1,6 +1,11 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MinecraftDownloader_v2
@@ -11,41 +16,10 @@ namespace MinecraftDownloader_v2
         {
             InitializeComponent();
         }
+
         private void MainWindow_Load(object sender, EventArgs e)
         {
 
-        }
-        private void Cancel_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void Install_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void Refresh_Click(object sender, EventArgs e)
-        {
-            GetVersions();
-        }
-        private void PathInsert_Click(object sender, EventArgs e)
-        {
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
-                return;
-            string pathSelected = folderBrowserDialog1.SelectedPath;
-            textBox1.Text = pathSelected;
-        }
-        private void GetVersions()
-        {
-            //Uri pathMeta = new Uri("https://wdfiles.ru/folder/8343/modpacks/meta");
-            Uri pathMeta = new Uri("https://wdfiles.ru/f12966");
-            string pathVersions = "versions.txt";
-            WebClient client = new WebClient();
-            client.DownloadFile(pathMeta, pathVersions);
-            StreamReader reader = new StreamReader(pathVersions);
-            string strVersions = reader.ReadToEnd();
-            listBox1.Items.Clear();
-            listBox1.Items.Add(strVersions);
-            reader.Close();
         }
     }
 }
